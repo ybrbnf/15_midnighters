b3cc3c839abff7655f2c15531bea5d759fdfef83
Nikita Tkachenko <ybrbnf418@gmail.com>
2016-10-19 14:29:17 +0300

    - Дополнен README.md - Использованы понятные названия переменных - Убрана глобальная переременная

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Скрипт выводит в консоль список пользователей, которые отправили задачи на проверку в промежуток между 00:00 и 6:00.
+
+
+Зависимости:
+
+- python 3
+- библиотека <a href = "http://docs.python-requests.org/en/master/">requests</a>
+- библиотека <a href = "https://pypi.python.org/pypi/pytz/">pytz</a>
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,31 +3,42 @@ import datetime
 from pytz import timezone
 
 
-def load_attempts():
-    url = 'http://devman.org/api/challenges/solution_attempts/'
-    number_of_pages = requests.get(url).json()['number_of_pages']
-    for page in range(1, number_of_pages):
-        key = {'page': page}
-        record = requests.get(url, params=key).json()['records']
-        for item in record:
-            yield item
+def get_number_of_pages(url):
+    return requests.get(url).json()['number_of_pages']
 
 
-def get_midnighters():
-    users = set()
+def get_records(number_of_pages, url):
+    paged_list = []
+    for number_of_page in range(1, number_of_pages):
+        key = {'page': number_of_page}
+        response = requests.get(url, params=key).json()['records']
+        paged_list.append(response)
+    return paged_list
+
+
+def get_attempts(paged_list):
+    list_attempts = []
+    for item in paged_list:
+        list_attempts.extend(item)
+    return list_attempts
+
+
+def get_midnighters(attempts):
+    midnighters = set()
     midnight = datetime.time(0)
     morning = datetime.time(6)
-    for item in data:
-        if item['timestamp']:
-            tz = timezone(item['timezone'])
-            srv_time = item['timestamp']
+    for attempt in attempts:
+        if attempt['timestamp']:
+            tz = timezone(attempt['timezone'])
+            srv_time = attempt['timestamp']
             user_time = tz.localize(datetime.datetime.fromtimestamp(srv_time))
-            if user_time.time() > midnight and user_time.time() < morning:
-                users.add(item['username'])
-    return users
-
+            if midnight < user_time.time() < morning:
+                midnighters.add(attempt['username'])
+    return midnighters
 
 if __name__ == '__main__':
-    data = load_attempts()
-    users = get_midnighters()
-    print (users)
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    number_of_pages = get_number_of_pages(url)
+    paged_list = get_records(number_of_pages, url)
+    attempts = get_attempts(paged_list)
+    print (get_midnighters(attempts))
